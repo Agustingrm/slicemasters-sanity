cb6f8b04899f270c3d6a87deb7e205afefb1bd47
Agustin Gramajo <agustingramajo@gmail.com>
2022-01-18 20:42:27 -0300

    first commit

---
+++ schemas/person.js
@@ -0,0 +1,41 @@
+import { MdPerson as icon } from 'react-icons/md';
+
+export default {
+  // Computer Name
+  name: 'person',
+  // Visible title
+  title: 'Name',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Pizza Name',
+      type: 'string',
+      description: 'Name of the pizza',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      description: 'Tell us a bit about this person',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
